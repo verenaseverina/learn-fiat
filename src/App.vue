3ee3e85c2e9b2290e39a7112e79493a5699d44b0
Verena Severina <wb-verenasev448872@antfin.com>
2021-03-16 13:12:54 +0700

    init project

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <input>
+    <button @click="checkBill">CHECK BILL</button>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Test App',
+  methods: {
+    checkBill () {
+      console.log('check bill')
+    },
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
