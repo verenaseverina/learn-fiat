49e6d88bf0c099a88ac2654b7bd547094d05c080
Jeffrey Marcelino <guemarcelino123@email.com>
2021-03-17 23:59:45 +0700

    Buat input bank, in put nominal, dan active deactive button

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,39 +1,28 @@
 <template>
   <div class="p-16">
 
-    <header class="head">
-      <div class="text-white bg-blue-50 text-white--bg-blue-50--custom">
-        <div class="f-title title-14 f-title--title-14--custom">
+      <div class="text-white bg-blue-50">
+        <div class="f-title title-14 inverted p-10">
           Panin
         </div>
       </div>
-    </header>
 
-    <body>
+    <div class="f-caption caption-12 secondary">
+        <div class="mb-6">CREDIT CARD NUMBER</div>
+        <f-input :maxlength="19" :errorMsg="remainDigit" format="{4} {4} {4} {4}" type="digital" v-model="bankCard"
+          @input="onChange" placeholder="Example 1234 5678 9012 3456" />
+    </div>
 
-      <section class="seg1">
-        <div class="f-caption caption-12 f-caption--caption-12--custom">
-          CREDIT CARD NUMBER <span @click="support">?</span>
-          <f-input :maxlength="19" :errorMsg="remainDigit" format="{4} {4} {4} {4}" type="digital" v-model="bankCard"
-            @input="onChange" placeholder="Example 1234 5678 9012 3456" />
-        </div>
-      </section>
-
-      <section class="seg2">
-        <div class="f-caption caption-12 f-caption--caption-12--custom2">
-          PAYMENT AMOUNT
-          <div class="moneyBox">
-            <f-input-money placeholder="50.000" ref="input" v-model="amount" :min-amount="currentPayerMinAmount" @input="onChange2">
-              <template slot="icon-outer-left">
-                <div class="f-input__icon-outer-right mr-12 f-input__icon-outer-right--custom">
-                  <img src="//a.m.dana.id/resource/imgs/fiat/currency.svg?t=1" />
-                </div>
-              </template>
-            </f-input-money>
-          </div>
-        </div>
-      </section>
-    </body>
+    <div class="f-caption caption-12">
+        <div class="mb-6">PAYMENT AMOUNT</div>
+          <f-input-money placeholder="50.000" ref="input" v-model="amount" :min-amount="currentPayerMinAmount" @input="onChange2">
+            <template slot="icon-outer-left">
+              <div class="f-input__icon-outer-right f-input__icon-outer-right--custom mr-12">
+                <img src="//a.m.dana.id/resource/imgs/fiat/currency.svg?t=1" />
+              </div>
+            </template>
+          </f-input-money>
+      </div>
 
     <f-btn :disabled='isDisabled' type="primary" size="large">
       CHECK BILL
@@ -42,21 +31,13 @@
   </div>
 </template>
 
-
-
 <script>
-  import {
-    FBtn,
-    FInput,
-    FInputMoney
-  } from '@dana/fiat'
-  export default {
-    components: {
-      FBtn,
-      FInput,
-      FInputMoney
-    },
-    data() {
+import { FBtn, FInput, FInputMoney } from '@dana/fiat'
+export default {
+  components: {
+    FBtn,FInput, FInputMoney
+  },
+  data() {
       return {
         dataSet: {
           bankCard: false,
@@ -69,27 +50,26 @@
         remainDigit: ""
       }
     },
-    methods: {
-      onChange(event) {
-        console.log('on event')
-        if (this.bankCard.length >= 1 && this.bankCard.length < 19) {
-          this.remainDigit = "The credit card number must be 12 digits"
-        } else if(this.bankCard.length < 19){
-          this.dataSet.bankCard = true;
-        }else{
-          this.remainDigit = "";
-        }
-      },
-      onChange2(event) {
-        if(parseInt(this.amount.replaceAll('.','')) >= 50000){
-          this.dataSet.amount = true;
-        }
-        else{
-          this.dataSet.amount = false;
-        }
+  methods: {
+    onChange(event) {
+      if (this.bankCard.length > 0 && this.bankCard.length < 19) {
+        this.remainDigit = "The credit card number must be 12 digits"
+      } else if(this.bankCard.length == 19){
+        this.dataSet.bankCard = true;
+      }else{
+        this.remainDigit = "";
       }
     },
-    computed: {
+    onChange2(event) {
+      if(parseInt(this.amount.replaceAll('.','')) >= 50000){
+        this.dataSet.amount = true;
+      }
+      else{
+        this.dataSet.amount = false;
+      }
+    }
+  },
+  computed: {
       isDisabled(){
         if(parseInt(this.amount.replaceAll('.','')) >= 50000 && this.bankCard.length == 19){
           return !this.dataSet.isDisabled
@@ -97,21 +77,19 @@
         return this.dataSet.isDisabled
       }
     }
-  }
+}
 </script>
 
-
 <style lang="less" scoped>
-  .f-input__icon-outer-right {
-    width: 0.24rem;
-    height: 0.24rem;
-    margin-top: 0.26rem;
-  }
-
-  .f-input__icon-outer-right--custom img {
+.f-input__icon-outer-right {
+  width: 0.24rem;
+  height: 0.24rem;
+  margin-top: 0.26rem;
+}
+.f-input__icon-outer-right--custom img {
     display: block;
     width: 100%;
     height: 100%;
-    margin: -33px 0 0 -20px;
+    margin: -33px 0 0 0px;
   }
 </style>
